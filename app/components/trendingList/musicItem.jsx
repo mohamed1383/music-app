37bc26f9ef399295d626d3a6d5c102fdd7b87eee
mohamed1383 <103193197+mohamed1383@users.noreply.github.com>
2025-09-09 22:43:39 +0330

    Dynamization-trendlist

--- app/components/trendingList/musicItem.jsx
+++ app/components/trendingList/musicItem.jsx
@@ -1,19 +1,22 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function MusicItem() {
+export default function MusicItem({ id, name, artist, isSelected, setIsSelected }) {
   return (
-    <div className="flex w-[80%] h-[75px] hover:bg-[#ffffff14] transition-all duration-200 ease-[cubic-bezier(.2,.8,.2,1)]
-    hover:border-[#ffffff1f] rounded-[10px] cursor-pointer">
+    <div className={`flex w-[80%] h-[75px] ${isSelected ? "border-[#ff6a0033] border bg-[#ff6a001A] " : "hover:bg-[#ffffff14] hover:border-[#ffffff1f]"}  transition-all duration-200 ease-[cubic-bezier(.2,.8,.2,1)]
+     rounded-[10px] cursor-pointer`} onClick={() => {
+        setIsSelected(id)
+        console.log('sds')
+      }}>
 
       <div className="flex items-center gap-[15px] ml-[20px]">
-        <p>1</p>
+        <p className={`${isSelected ? 'text-[#ff6a00]' : ''}`}>{id}</p>
         <Image width={48} height={48}></Image>
       </div>
       <div className="flex w-[70%] justify-between items-center ml-[20px] pr-[10px]">
         <div className="flex flex-col">
-            <p className="">Retro Future</p>
-            <p className="">Synthwave Collective</p>
+          <p className={`${isSelected ? "text-[#ff6a00]" : ""}`}>{name}</p>
+          <p className="">{artist}</p>
         </div>
         <p>3:54</p>
       </div>
@@ -21,5 +24,3 @@ export default function MusicItem() {
   )
 }
 
-
-//  selectedStyle: bg-[#ff6a001A] border-[#ff6a0033] musicName: text-[#ff6a00]
