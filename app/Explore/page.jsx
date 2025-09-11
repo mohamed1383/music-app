7c19e7b6c84e31183b1ed6289f02fd7671820c40
mohamed1383 <103193197+mohamed1383@users.noreply.github.com>
2025-09-11 13:23:06 +0330

    Completion-main-page

--- app/Explore/page.jsx
+++ app/Explore/page.jsx
@@ -18,7 +18,14 @@ export default function page() {
       <Slider></Slider>
       <List></List>
       <h1 className={`absolute transition-all duration-300 top-[45%] ${menuSmallSize ? "left-[7.5%]" : "left-[22.5%]"} text-[35px]`}>Recently Added</h1>
-      <div className={`absolute mt-10 flex gap-12 transition-all duration-300 top-[50%] min-h-[30%] ${menuSmallSize ? "w-[63%]  left-[7.5%]" : "w-[50%]  left-[22.5%]"} rounded-[10px]`}>
+      <div className={`absolute mt-10 mb-5 flex gap-12 transition-all duration-300 top-[50%] min-h-[30%] ${menuSmallSize ? "w-[63%]  left-[7.5%]" : "w-[50%]  left-[22.5%]"} rounded-[10px]`}>
+        <MusicCard></MusicCard>
+        <MusicCard></MusicCard>
+        <MusicCard></MusicCard>
+      </div>
+
+      <h1 className={`absolute mt-14 transition-all duration-300 top-[97%] ${menuSmallSize ? "left-[7.5%]" : "left-[22.5%]"} text-[35px]`}>popular</h1>
+      <div className={`absolute pb-10 mt-24 flex gap-12 transition-all duration-300 top-[100%] min-h-[30%] ${menuSmallSize ? "w-[63%]  left-[7.5%]" : "w-[50%]  left-[22.5%]"}`}>
         <MusicCard></MusicCard>
         <MusicCard></MusicCard>
         <MusicCard></MusicCard>
