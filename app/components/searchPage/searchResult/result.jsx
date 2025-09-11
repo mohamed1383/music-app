bec6d066804f764d19543d630a1787c3fa0a3e35
mohamed1383 <103193197+mohamed1383@users.noreply.github.com>
2025-09-11 21:53:23 +0330

    add-dynamic-size

--- app/components/searchPage/searchResult/result.jsx
+++ app/components/searchPage/searchResult/result.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+"use client"
+
+import React,{useContext} from 'react'
 import MusicCard from '../../section/musicCard'
+import { MenuCotext } from '@/app/context/publicDatas'
 
 export default function Result() {
 
+    const {menuSmallSize} = useContext(MenuCotext)
+
+    console.log(menuSmallSize)
+
     return (
-        <div className='absolute w-[80%] right-0 top-[30%] pl-[35px] pr-[35px]'>
+        <div className={`absolute transition-all duration-300 ${menuSmallSize ? "w-[95%]" : "w-[80%]"} right-0 top-[30%] pl-[35px] pr-[35px] pb-10`}>
             <hr className='text-[#ffffff16] mb-10'/>
             <div className="flex gap-[80px] flex-wrap">
                 {[...Array(6)].map(() => {
-                   console.log("ss") 
                    return <MusicCard></MusicCard>
                 })}
             </div>
